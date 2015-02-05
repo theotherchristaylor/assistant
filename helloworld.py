2afdc4860dfea225f8f191b1f555bbf3d401a188
Chris Taylor <theotherchristaylor@gmail.com>
2015-02-04 20:26:00 -0700

    Added config.txt credentials file

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,14 @@
 import gdata.docs.service
 
+config = file.open('config.txt', 'r')
+
+user = config.readline()
+password = config.readline()
+
 # Create a client class which will make HTTP request with Google Docs server
 client = gdata.docs.service.DocsService()
 # Authenticate using your email address and password
-client.ClientLogin('jaspertheassistant@gmail.com', 'JasperAssistant')
+client.ClientLogin(user, password)
 
 # Query the server for an Atom feed containing a  list of documents
 documents_feed = client.GetDocumentListFeed()
